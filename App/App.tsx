ec0da66e1937fd0fa151442316cf426d3a66469d
Hyuntak Lee <ttttaakk620@gmail.com>
2022-08-24 13:35:05 +0900

    [feat] study about device screen resolution

--- App/App.tsx
+++ App/App.tsx
@@ -1,117 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
+import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { DEVICE_HEIGHT, DEVICE_WIDTH } from './src/constants/theme';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+  
+  console.log('계산된 높이:'+ DEVICE_HEIGHT)
+  
+  return (  
+    <View>
+      <View style={styles.box_1}>
+        <Text>BOX_1</Text>
+      </View>
+      <View style={styles.box_2}>
+        <Text>BOX_1</Text>
+      </View>
+    </View>    
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  box_1: {
+    width: DEVICE_WIDTH * 50,
+    //width: '100%',
+    backgroundColor:'red',
+    borderWidth:3,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  box_2: {
+    width: DEVICE_WIDTH * 30,
+    //width: '100%',
+    backgroundColor:'red',
+    borderWidth:3,
+  }
 });
 
 export default App;
---
+++ App/src/constants/theme.ts
@@ -0,0 +1,18 @@
+import { Dimensions } from "react-native";
+
+//figma default size 
+export const basicDimensions = {
+    height: 640,
+    width: 360,
+};
+
+//calculate custom size
+export const DEVICE_HEIGHT = (
+    Dimensions.get('screen').height *
+    (1 / basicDimensions.height)
+).toFixed(2);
+
+export const DEVICE_WIDTH = (
+    Dimensions.get('screen').height *
+    (1 / basicDimensions.width)
+).toFixed(2);
